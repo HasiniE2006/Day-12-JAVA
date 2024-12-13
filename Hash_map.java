39f97e597b192017be19e68ba483ffd6ced0b05b
Hasini E <hasini1530@gmail.com>
2024-12-13 15:27:48 +0530

    Add files via upload

---
+++ Hash_map.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Hash_map {
+    public static void main(String[] args) {
+        HashMap <String,String> a=new HashMap<>();
+        a.put("sname","Hasini");
+        a.put("sroll_num","01");
+        System.out.println(a);
+    }
+}
